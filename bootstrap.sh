4d7a4914b9a96a4a36713ea8f97d9272a5758575
roninxmk <nakagomi@gmail.com>
2018-06-24 19:25:31 -0400

    Few changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export FLASK_APP=./app/app.py
+export FLASK_DEBUG=1
 # source /c/Users/Buster/.virtualenvs/REST-API-exercise-Oypl-3Vg/Scripts/activate
 source $(pipenv --venv)\\Scripts\\activate
 flask run -h 0.0.0.0
