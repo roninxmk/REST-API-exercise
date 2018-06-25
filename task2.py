4387b3f245e446fb4b592cd5dac1138d524d9ca4
roninxmk <nakagomi@gmail.com>
2018-06-24 20:27:59 -0400

    All tasks done

--- task2.py
+++ task2.py
@@ -24,6 +24,36 @@ def createRule(parameter, threshold, users):
     headers = {'Content-type': 'application/json'}
     response = requests.post(url, data=data_json, headers=headers)
     return response
+
+def editRule(parameter, threshold, users):
+    url = 'http://localhost:5000/api/v1/rules/' + parameter
+    print(url)
+    data = {
+        "parameter": parameter,
+        "threshold": threshold,
+        "usernames": users
+    }
+    data_json = json.dumps(data)
+    headers = {'Content-type': 'application/json'}
+    response = requests.put(url, data=data_json, headers=headers)
+    return response
+
+def createEvent(parameter, value):
+    url = 'http://localhost:5000/api/v1/events'
+    data = {
+        "parameter": parameter,
+        "value": value
+    }
+    data_json = json.dumps(data)
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(url, data=data_json, headers=headers)
+    return response
+
+def getUserAlert(user):
+    url = 'http://localhost:5000/api/v1/users/' + user
+    print(url)
+    response = requests.get(url)
+    return response
 	
 def main():
     response = createUser("rvitorino")
@@ -33,4 +63,17 @@ def main():
     response = createUser("andre")
     pprint.pprint(response.json())
 
+    response = createRule("CO", 20000, ["rvitorino", "fmonsanto"])
+    pprint.pprint(response.json())
+
+    response = editRule("CO", 25000, ["rvitorino", "fmonsanto"])
+    pprint.pprint(response.json())
+
+    response = createEvent("CO", 30000)
+    pprint.pprint(response.json())
+
+    response = getUserAlert("fmonsanto")
+    pprint.pprint(response.json())
+	
+	
 main()
